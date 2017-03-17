632ebe039552dd3d713f6764d6ee243bc4c684d2
James Chant <jamesrchant@gmail.com>
2017-03-17 07:43:07 +0000

    Added stag hill route and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
     def home
     end
+    
+    def stag_hill
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'stag-hill', to: 'pages#stag_hill'
 end
